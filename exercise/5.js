651a1be1f5070aea32c94329a151be2ea2250776
yolo310250 <36975360+yolo310250@users.noreply.github.com>
2018-03-30 18:09:45 +0800

    Add files via upload

---
+++ exercise/5.js
@@ -0,0 +1,10 @@
+var score = 61;
+
+if (score >= 90)
+  console.log("degree(61) => A");
+else if(score >=80)
+  console.log("degree(61) => B");
+else if(score >=70)
+  console.log("degree(61) => C");
+  else if(score <=69)
+  console.log("degree(61) => D");
